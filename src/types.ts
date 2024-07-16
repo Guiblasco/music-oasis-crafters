1ad44b629bbc6ac29f4e86a6e5694e46a510c680
Guillem Blasco <gowet92@gmail.com>
2024-07-16 19:44:42 +0200

    Add type Disc and discs collection

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export type Disc = {
+  id: number;
+  name: string;
+  artistName: string;
+  image: string;
+  discFormat: string;
+  pirce: number;
+  originalPrice: number;
+  isMonthDisc: boolean;
+};
